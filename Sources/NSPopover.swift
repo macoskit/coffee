7514c590609ad61ee5cfd2a1d71ec3be65396ea7
vaux <zero.griffin@gmail.com>
2022-08-10 15:41:02 +0200

    Update popover

--- Sources/NSPopover.swift
+++ Sources/NSPopover.swift
@@ -6,7 +6,7 @@ extension NSPopover {
         contentSize = content.frame.size
         contentViewController = .init()
         contentViewController!.view = content
-        show(relativeTo: on.bounds, of: on, preferredEdge: .minY)
+        show(relativeTo: on.bounds, of: on, preferredEdge: edge)
         contentViewController!.view.window!.makeKey()
     }
 }
