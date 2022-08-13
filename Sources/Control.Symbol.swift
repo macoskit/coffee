706288fe88dc9ff11e3b7edabb498a7f6f7e7c77
vaux <zero.griffin@gmail.com>
2022-08-13 17:39:52 +0200

    Update symbol

--- Sources/Control.Symbol.swift
+++ Sources/Control.Symbol.swift
@@ -2,29 +2,51 @@ import AppKit
 
 extension Control {
     public final class Symbol: Control {
-        public private(set) weak var image: NSImageView!
+        public var color = NSColor.secondaryLabelColor {
+            didSet {
+                update()
+            }
+        }
+        
+        public var symbol: String {
+            didSet {
+                update()
+            }
+        }
+        
+        private weak var image: NSImageView!
         private let background: Bool
+        private let size: CGFloat
+        private let weight: NSFont.Weight
         
         required init?(coder: NSCoder) { nil }
-        public init(symbol: String, size: CGFloat, background: Bool = true) {
+        public init(symbol: String,
+                    size: CGFloat,
+                    radius: CGFloat = 7,
+                    content: CGFloat = 30,
+                    weight: NSFont.Weight = .regular,
+                    background: Bool = true) {
+            self.symbol = symbol
+            self.size = size
+            self.weight = weight
             self.background = background
             
-            let image = NSImageView(image: .init(systemSymbolName: symbol,
-                                                 accessibilityDescription: nil) ?? .init())
+            let image = NSImageView()
             image.translatesAutoresizingMaskIntoConstraints = false
-            image.symbolConfiguration = .init(pointSize: size, weight: .regular)
-                .applying(.init(hierarchicalColor: .secondaryLabelColor))
+            image.imageScaling = .scaleNone
             self.image = image
             
             super.init(layer: true)
-            layer!.cornerRadius = 7
+            layer!.cornerRadius = radius
             layer!.cornerCurve = .continuous
             addSubview(image)
             
-            widthAnchor.constraint(equalToConstant: 30).isActive = true
-            heightAnchor.constraint(equalToConstant: 30).isActive = true
+            widthAnchor.constraint(equalToConstant: content).isActive = true
+            heightAnchor.constraint(equalToConstant: content).isActive = true
             image.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
             image.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+            image.widthAnchor.constraint(equalToConstant: content).isActive = true
+            image.heightAnchor.constraint(equalToConstant: content).isActive = true
         }
         
         public override func updateLayer() {
@@ -46,6 +68,12 @@ extension Control {
         
         public override var allowsVibrancy: Bool {
             true
+        }
+        
+        private func update() {
+            image.image = .init(systemSymbolName: symbol, accessibilityDescription: nil)
+            image.symbolConfiguration = .init(pointSize: size, weight: weight)
+                .applying(.init(hierarchicalColor: .secondaryLabelColor))
         }
     }
 }
