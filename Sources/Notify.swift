863aeba49e32d0559ddbffcd1220ff45dd4afd22
vaux <zero.griffin@gmail.com>
2022-08-20 17:19:51 +0200

    Add notify

---
+++ Sources/Notify.swift
@@ -0,0 +1,30 @@
+import AppKit
+
+public class Notify: NSWindow {
+    public init(size: CGSize) {
+        super.init(contentRect: .init(origin: .zero, size: size),
+                   styleMask: [.titled, .fullSizeContentView], backing: .buffered, defer: true)
+        animationBehavior = .alertPanel
+        isReleasedWhenClosed = false
+        titlebarAppearsTransparent = true
+        
+        let content = NSVisualEffectView()
+        content.state = .active
+        content.material = .hudWindow
+        contentView = content
+        center()
+    }
+    
+    public final override func keyDown(with: NSEvent) {
+        switch with.keyCode {
+        case 36:
+            close()
+        default:
+            super.keyDown(with: with)
+        }
+    }
+    
+    public final override func cancelOperation(_: Any?) {
+        close()
+    }
+}
