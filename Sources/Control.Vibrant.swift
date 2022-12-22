22c983a4d92f92e05adbfc9fcbb59e8419652361
Vicente Garcia <zero.griffin@gmail.com>
2022-12-22 15:20:55 +0100

    Add vibrant

---
+++ Sources/Control.Vibrant.swift
@@ -0,0 +1,35 @@
+import AppKit
+
+extension Control {
+    public final class Vibrant: Control {
+        required init?(coder: NSCoder) { nil }
+        public init(title: String, radius: CGFloat = 8) {
+            let text = Text(vibrancy: false)
+            text.stringValue = title
+            text.font = .systemFont(ofSize: NSFont.preferredFont(forTextStyle: .body).pointSize, weight: .medium)
+            text.textColor = .labelColor
+            
+            super.init(layer: true)
+            layer!.cornerRadius = radius
+            addSubview(text)
+            
+            text.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+            text.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        }
+        
+        public override func updateLayer() {
+            super.updateLayer()
+            
+            NSApp
+                .effectiveAppearance
+                .performAsCurrentDrawingAppearance {
+                    switch state {
+                    case .highlighted, .pressed:
+                        layer!.backgroundColor = NSColor.quaternaryLabelColor.cgColor
+                    default:
+                        layer!.backgroundColor = NSColor.tertiaryLabelColor.cgColor
+                    }
+                }
+        }
+    }
+}
