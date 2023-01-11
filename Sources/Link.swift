fe4f3490dcd6fa928322e066746f758843f70961
Vicente Garcia <vicegax@gmail.com>
2023-01-11 16:53:53 +0100

    Update share

--- Sources/Link.swift
+++ Sources/Link.swift
@@ -1,19 +1,22 @@
 import AppKit
 
 public final class Link: NSMenuItem {
-    private let link: String
+    private let link: String?
     
     required init(coder: NSCoder) { fatalError() }
-    public init(title: String, link: String) {
+    public init(title: String, link: String?) {
         self.link = link
         
         super.init(title: title, action: nil, keyEquivalent: "")
+        image = .init(systemSymbolName: "link", accessibilityDescription: "URL")
+        
+        guard link != nil else { return }
         target = self
         action = #selector(share)
-        image = .init(systemSymbolName: "link", accessibilityDescription: "URL")
     }
     
     @objc private func share() {
+        guard let link else { return }
         NSPasteboard.general.clearContents()
         NSPasteboard.general.setString(link, forType: .string)
     }
--- Sources/Share.swift
+++ Sources/Share.swift
@@ -1,14 +1,18 @@
 import AppKit
 
 public final class Share: NSMenuItem {
-    private let url: URL
+    private let url: URL?
     
     required init(coder: NSCoder) { fatalError() }
-    public init(title: String, url: URL) {
+    public init(title: String, url: URL?) {
         self.url = url
         
         super.init(title: title, action: nil, keyEquivalent: "")
+        image = .init(systemSymbolName: "square.and.arrow.up", accessibilityDescription: title)
         submenu = .init(title: title)
+        
+        guard let url else { return }
+        
         submenu!.items = [
             .item(url.absoluteString.prefix(34) + "..."),
             .separator()]
@@ -21,10 +25,10 @@ public final class Share: NSMenuItem {
                         .with(image: service.image)
                         .with(object: service)
                     }
-        image = .init(systemSymbolName: "square.and.arrow.up", accessibilityDescription: title)
     }
     
     @objc private func share(_ item: NSMenuItem) {
+        guard let url else { return }
         guard let service = item.representedObject as? NSSharingService else { return }
         service.perform(withItems: [url])
     }
