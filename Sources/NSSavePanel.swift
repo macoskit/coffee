506d795d9203042b36f17102b48a2df1ca640ef2
Vicente Garcia <vicegax@gmail.com>
2023-01-12 12:50:16 +0100

    Update save panel

---
+++ Sources/NSSavePanel.swift
@@ -0,0 +1,31 @@
+import AppKit
+import UniformTypeIdentifiers
+
+extension NSSavePanel {
+    public func with(name: String) -> Self {
+        nameFieldStringValue = name
+        return self
+    }
+    
+    public func with(types: [UTType]) -> Self {
+        allowedContentTypes = types
+        return self
+    }
+    
+    public func save(data: Data) async -> Bool {
+        await withUnsafeContinuation { continuation in
+            begin { [weak self] response in
+                guard
+                    response == .OK,
+                    let url = self?.url,
+                    let _ = try? data.write(to: url, options: .atomic)
+                else {
+                    continuation.resume(returning: false)
+                    return
+                }
+                NSWorkspace.shared.activateFileViewerSelecting([url])
+                continuation.resume(returning: true)
+            }
+        }
+    }
+}
